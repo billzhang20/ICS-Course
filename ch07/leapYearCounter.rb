b5fdc09bf1777ac7557ebe05784f687e1db36ed7
Bill Zhang <billzhang20@gmail.com>
2017-06-15 10:24:11 +0800

    Turning in Homework

--- ch07/leapYearCounter.rb
+++ ch07/leapYearCounter.rb
@@ -1,19 +1,17 @@
 # leap year counter
 puts("Please enter desired starting year:")
-start = gets.chomp.to_f
+start = gets.chomp.to_i
 puts("Please enter desired ending year:")
-finish = gets.chomp.to_f
+finish = gets.chomp.to_i
 puts("Here are the leap years in your desired range...")
 
 number = start
 while number <= finish
   if number%4 == 0 and number%100 != 0
-    nice_number = number.to_i
-    puts(nice_number)
+    puts(number)
     number = number+1
   elsif number%400 == 0
-    nice_number = number.to_i
-    puts(nice_number)
+    puts(number)
     number = number+1
   else
     number = number+1

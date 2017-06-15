f90d598745c6dff0fbd8ad2ef530c954e2c65105
Bill Zhang <billzhang20@gmail.com>
2017-06-15 15:28:42 +0800

    Turning in Homework

--- ch08/tableOfContents.rb
+++ ch08/tableOfContents.rb
@@ -1,8 +1,9 @@
 # table of contents revisited
 
-toc = ["Chapter 1: Getting Started -- page 1", "Chapter 2: Numbers -- page 9", "Chapter 3: Letters -- page 13"]
+toc = [["Chapter 1: Getting Started".ljust(20), "page 1".rjust(20)],
+   ["Chapter 2: Numbers".ljust(20), "page 9".rjust(26)], ["Chapter 3: Letters".ljust(20), "page 13".rjust(27)]]
 puts("Table of Contents".center(40))
 sorted_list = toc.sort
-sorted_list.each do |x|
-  puts(x)
+sorted_list.each do |x, y|
+  puts(x+y)
 end

ea68b1f734e593acd249ad6a6d1172d86240417b
Bill Zhang <billzhang@Bills-MacBook-Pro.local>
2017-06-29 13:53:03 -0500

    Turning in Homework

---
+++ ch13/interactiveDragon.rb
@@ -0,0 +1,147 @@
+class Dragon
+  def initialize name
+    @name = name
+    @asleep = false
+    @stuff_in_belly = 10
+    @stuff_in_intestine = 0
+
+    puts "#{@name} is born."
+  end
+
+  def feed
+    puts "You feed #{@name}."
+    @stuff_in_belly = 10
+    passage_of_time
+  end
+
+  def walk
+    puts "You walk #{@name}."
+    @stuff_in_intestine = 0
+    passage_of_time
+  end
+
+  def put_to_bed
+    puts "You put #{@name} to bed."
+    @asleep = true
+    3.times do
+      if @asleep
+        passage_of_time
+      end
+      if @asleep
+        puts "#{@name} snores, filling the room with smoke."
+      end
+    end
+    if @asleep
+      @asleep = false
+      puts "#{@name} wakes up slowly."
+    end
+  end
+
+  def toss
+      puts "You toss #{@name} up into the air."
+      puts 'He giggles, which singes your eyebrows.'
+      passage_of_time
+  end
+
+  def rock
+    puts "You rock #{@name} gently."
+    @asleep = true
+    puts 'He briefly dozes off...'
+    passage_of_time
+    if @asleep
+      @asleep = false
+      puts '...but wakes when you stop.'
+    end
+  end
+
+  private
+
+  def hungry?
+# Method names can end with "?".
+# Usually, we do this only if the method # returns true or false, like this:
+    @stuff_in_belly <= 2
+  end
+
+  def poopy?
+    @stuff_in_intestine >= 8
+  end
+
+  def passage_of_time
+    if @stuff_in_belly > 0
+    #  Move food from belly to intestine.
+      @stuff_in_belly     = @stuff_in_belly     - 1
+      @stuff_in_intestine = @stuff_in_intestine + 1
+    else # Our dragon is starving!
+      if @asleep
+        @asleep = false
+        puts 'He wakes up suddenly!'
+      end
+      puts "#{@name} is starving!  In desperation, he ate YOU!"
+      exit # This quits the program.
+    end
+
+    if @stuff_in_intestine >= 10
+      @stuff_in_intestine = 0
+      puts "Whoops! #{@name} had an accident..."
+    end
+
+    if hungry?
+      if @asleep
+        @asleep = false
+        puts 'He wakes up suddenly!'
+      end
+      puts "#{@name}'s stomach grumbles..."
+    end
+
+    if poopy?
+      if @asleep
+        @asleep = false
+        puts 'He wakes up suddenly!'
+      end
+      puts "#{@name} does the potty dance..."
+    end
+  end
+end
+
+
+puts("What would you like to name your dragon? ")
+name = gets.chomp
+
+dragon1 = Dragon.new name 
+
+puts("What would you like to do with your dragon? (Feed, Walk, Put to Bed, Toss, Rock, Exit)")
+answer = gets.chomp
+
+while answer != "Exit"
+  if answer == "Feed"
+    dragon1.feed
+    puts("What would you like to do with your dragon? (Feed, Walk, Put to Bed, Toss, Rock, Exit)")
+    answer = gets.chomp
+
+  elsif answer == "Walk"
+    dragon1.walk
+    puts("What would you like to do with your dragon? (Feed, Walk, Put to Bed, Toss, Rock, Exit)")
+    answer = gets.chomp
+
+  elsif answer == "Put to Bed"
+    dragon1.put_to_bed
+    puts("What would you like to do with your dragon? (Feed, Walk, Put to Bed, Toss, Rock, Exit)")
+    answer = gets.chomp
+
+  elsif answer == "Toss"
+    dragon1.toss
+    puts("What would you like to do with your dragon? (Feed, Walk, Put to Bed, Toss, Rock, Exit)")
+    answer = gets.chomp
+
+  elsif answer == "Rock"
+    dragon1.rock
+    puts("What would you like to do with your dragon? (Feed, Walk, Put to Bed, Toss, Rock, Exit)")
+    answer = gets.chomp
+
+  else
+    puts("INVALID INPUT. Try again!")
+    puts("What would you like to do with your dragon? (Feed, Walk, Put to Bed, Toss, Rock, Exit)")
+    answer = gets.chomp
+
+  end
+end
---
+++ ch13/orangeTree.rb
@@ -0,0 +1,79 @@
+class OrangeTree
+  def initialize
+    @age = 0
+    @height = 0
+    @fruit = 0
+
+    puts('An Orange tree has been planted!')
+  end
+
+  def one_year_passes
+    puts('One year has passed')
+    @fruit = 0
+    @age = @age + 1
+    @height = @height + 1
+    if @age > 3
+      grow_fruit
+    end
+    if @age == 10
+      puts("Sorry, the tree died.")
+      exit
+    end
+  end
+
+  def pick_an_orange
+    if @fruit == 0
+      puts("Sorry, there are no oranges on the tree!")
+    else
+      puts("Yum! What a delicious orange!")
+      @fruit = @fruit - 1
+    end
+  end
+
+  def height
+    return @height
+  end
+
+  def count_the_oranges
+    return @fruit
+  end
+
+  private
+
+  def grow_fruit
+    @fruit = @fruit + @age
+  end
+end
+
+tree = OrangeTree.new
+
+puts("What would you like to do? (Grow, Pick, Count, Height, Exit)")
+answer = gets.chomp
+
+while answer != "Exit"
+  if answer == "Grow"
+    tree.one_year_passes
+    puts("What would you like to do? (Grow, Pick, Count, Height, Exit)")
+    answer = gets.chomp
+
+  elsif answer == "Pick"
+    tree.pick_an_orange
+    puts("What would you like to do? (Grow, Pick, Count, Height, Exit)")
+    answer = gets.chomp
+
+  elsif answer == "Count"
+    puts(tree.count_the_oranges)
+    puts("What would you like to do? (Grow, Pick, Count, Height, Exit)")
+    answer = gets.chomp
+
+  elsif answer == "Height"
+    puts(tree.height)
+    puts("What would you like to do? (Grow, Pick, Count, Height, Exit)")
+    answer = gets.chomp
+
+  else
+    puts("Invalid Input. Try again.")
+    puts("What would you like to do? (Grow, Pick, Count, Height, Exit)")
+    answer = gets.chomp
+  end
+end
